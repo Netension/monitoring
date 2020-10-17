be0c2bb2bd334613361b8f48281dde172bb0b370
Bence Mihucza <Mihben@gmail.com>
2020-10-17 20:38:17 +0200

    Implement example.

---
+++ src/Examples/Netension.Monitoring.Example.NetCoreWebApplication/Controllers/ExampleController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Netension.Monitoring.Prometheus;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Netension.Monitoring.Example.NetCoreWebApplication.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        private readonly ICounterManager _counterManager;
+        private readonly IGaugeManager _gaugeManager;
+        private readonly IHistogramManager _histogramManager;
+        private readonly ISummaryManager _summaryManager;
+
+        public ExampleController(ICounterManager counterManager, IGaugeManager gaugeManager, IHistogramManager histogramManager, ISummaryManager summaryManager)
+        {
+            _counterManager = counterManager;
+            _gaugeManager = gaugeManager;
+            _histogramManager = histogramManager;
+            _summaryManager = summaryManager;
+        }
+
+        [HttpGet]
+        [Route("counter-increase")]
+        public IActionResult CounterIncrease()
+        {
+            _counterManager["example_counter_metric"].Inc();
+            _counterManager.Increase("example_counter_metric_without_label");
+            _counterManager.Increase("example_counter_metric_with_label", 2.0, "label1", "label2");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("counter-set")]
+        public IActionResult CounterSet()
+        {
+            _counterManager["example_counter_metric"].IncTo(5.0);
+            _counterManager.Set("example_counter_metric", 5.0);
+            _counterManager.Set("example_counter_metric_with_label", 5.0, "label1", "label2");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("gauge-increase")]
+        public IActionResult GaugeIncrease()
+        {
+            _gaugeManager["example_gauge_metric"].Inc();
+            _gaugeManager.Increase("example_gauge_metric_without_label");
+            _gaugeManager.Increase("example_gauge_metric_with_label", 2.0, "label1", "label2");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("gauge-decrease")]
+        public IActionResult GaugeDecrease()
+        {
+            _gaugeManager["example_gauge_metric"].Dec();
+            _gaugeManager.Decrease("example_gauge_metric_without_label");
+            _gaugeManager.Decrease("example_gauge_metric_with_label", 2.0, "label1", "label2");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("gauge-set")]
+        public IActionResult GaugeSet()
+        {
+            _gaugeManager.Set("example_gauge_metric", 5.0);
+            _gaugeManager.Set("example_gauge_metric_with_label", 5.0, "label1", "label2");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("histogram_observe")]
+        public IActionResult HistogramObserve()
+        {
+            _histogramManager["example_histogram_metric"].Observe(2.0);
+            _histogramManager.Observe("example_histogram_metric_without_label", 2.0);
+            _histogramManager.Observe("example_histogram_metric_with_label", 2.0, "label1", "label2");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("summary_observe")]
+        public IActionResult SummaryObserve()
+        {
+            _summaryManager["example_summary_metric"].Observe(2.0);
+            _summaryManager.Observe("example_summary_metric_without_label", 2.0);
+            _summaryManager.Observe("example_summary_metric_with_label", 2.0, "label1", "label2");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("summary_timing")]
+        public async Task<IActionResult> SummaryTiming()
+        {
+            using (var stopwatch = _summaryManager.MeasureDuration("example_summary_metric_without_label"))
+            {
+                await Task.Delay(TimeSpan.FromSeconds(3));
+            }
+
+            using (var stopwatch = _summaryManager.MeasureDuration("example_summary_metric_with_label", "label1", "label2"))
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+            }
+            return Ok();
+        }
+    }
+}
--- src/Examples/Netension.Monitoring.Example.NetCoreWebApplication/Startup.cs
+++ src/Examples/Netension.Monitoring.Example.NetCoreWebApplication/Startup.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Netension.Monitoring.Prometheus;
 using Prometheus;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
 
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.14")]
 namespace Netension.Monitoring.Example.NetCoreWebApplication
 {
     public class Startup
@@ -15,10 +17,35 @@ namespace Netension.Monitoring.Example.NetCoreWebApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddOpenApiDocument(settings =>
+            {
+                var version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+                settings.DocumentName = $"v{version}";
+                settings.Title = "Netension.Monitoring.Prometheus .NET Core Example";
+                settings.Version = version;
+            });
+
             services.AddPrometheusMetrics((registry, provider) =>
             {
-                registry
-                    .RegistrateCounter("example_counter_metric", "Example counter metric.");
+                registry.RegistrateCounter(Metrics.CreateCounter("example_counter_metric", "Example counter metric."));
+                registry.RegistrateCounter("example_counter_metric_without_label", "Example counter metric without label.");
+                registry.RegistrateCounter("example_counter_metric_with_label", "Example counter metric with label.", new List<string> { "label1", "label2" });
+
+                registry.RegistrateGauge(Metrics.CreateGauge("example_gauge_metric", "Example gauge metric."));
+                registry.RegistrateGauge("example_gauge_metric_without_label", "Example gauge metric without label.");
+                registry.RegistrateGauge("example_gauge_metric_with_label", "Example gauge metric with label.", new List<string> { "label1", "label2" });
+
+
+                registry.RegistrateHistogram(Metrics.CreateHistogram("example_histogram_metric", "Example histogram metric."));
+                registry.RegistrateHistogram("example_histogram_metric_without_label", "Example histogram metric without label.", new double[] { 1.0, 2.0, 3.0 });
+                registry.RegistrateHistogram("example_histogram_metric_with_label", "Example histogram metric with label.", new double[] { 1.0, 2.0, 3.0 }, new List<string> { "label1", "label2" });
+
+                registry.RegistrateSummary(Metrics.CreateSummary("example_summary_metric", "Example summary metric."));
+                registry.RegistrateSummary("example_summary_metric_without_label", "Example summary metric without label.");
+                registry.RegistrateSummary("example_summary_metric_with_label", "Example summary metric with label.", new List<string> { "label1", "label2" });
             });
         }
 
@@ -32,14 +59,19 @@ namespace Netension.Monitoring.Example.NetCoreWebApplication
 
             app.UseRouting();
 
+            app.UseOpenApi(settings =>
+            {
+                settings.DocumentName = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            });
+            app.UseSwaggerUi3(settings =>
+            {
+                settings.Path = string.Empty;
+            });
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    counterManager.Increase("example_counter_metric");
-                    await context.Response.WriteAsync("Hello World!");
-                });
                 endpoints.MapMetrics();
+                endpoints.MapControllers();
             });
         }
     }
